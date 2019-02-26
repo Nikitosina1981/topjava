bed1e7956c33ee7dde2e1aa99349a18390cbc506
Nikita <nikita.krutov@gmail.com>
2019-02-26 18:52:38 +0500

    HW3 mandatory task done

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -5,11 +5,11 @@ import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class Meal extends AbstractBaseEntity {
-    private final LocalDateTime dateTime;
+    private LocalDateTime dateTime;
 
-    private final String description;
+    private String description;
 
-    private final int calories;
+    private int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -21,6 +21,14 @@ public class Meal extends AbstractBaseEntity {
         this.description = description;
         this.calories = calories;
     }
+
+    public void setDateTime(LocalDateTime localDateTime) {dateTime = localDateTime;}
+
+    public void setDescription(String description) {this.description = description;}
+
+    public void setCalories(int calories) {this.calories = calories;}
+
+    private Meal(){}
 
     public LocalDateTime getDateTime() {
         return dateTime;
@@ -41,6 +49,7 @@ public class Meal extends AbstractBaseEntity {
     public LocalTime getTime() {
         return dateTime.toLocalTime();
     }
+
 
     @Override
     public String toString() {
